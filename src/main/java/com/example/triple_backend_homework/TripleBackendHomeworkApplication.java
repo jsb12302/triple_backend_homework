05c16e5dc78fa41995798f8d990c08e0876c714b
정순범 <soonbum@jeongsunbeom-ui-MacBookPro.local>
2022-06-28 23:07:53 +0900

    initial commit

---
+++ src/main/java/com/example/triple_backend_homework/TripleBackendHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.triple_backend_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleBackendHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleBackendHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/triple_backend_homework/TripleBackendHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triple_backend_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleBackendHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
